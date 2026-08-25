a7a53fdaf988fd24be4b30f2553420b7630f6e0c
sharmakhusboo6419-hue <sharmakhusboo6419@gmail.com>
2026-08-25 22:55:16 +0530

    push the files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Server is running'));
 
